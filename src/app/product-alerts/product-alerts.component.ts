778af4751800bfbf3b8a5642b0494e16a9190b6c
lucasformaggine <lucasformaggine@hotmail.com>
2021-04-26 16:01:29 -0300

    Added "product-alerts" component

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-product-alerts',
-  templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  selector: "app-product-alerts",
+  templateUrl: "./product-alerts.component.html",
+  styleUrls: ["./product-alerts.component.css"]
 })
 export class ProductAlertsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
